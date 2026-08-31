e243c48a2a1d970e9aedee52712ba0b653780894
laphisboy <laphisboy@gmail.com>
2026-08-31 13:31:18 +0900

    Clean up

--- pages/_app.js
+++ pages/_app.js
@@ -9,9 +9,6 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Young Sun Choi - Ph.D. student at Yonsei University, 3D computer vision." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icon-32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icon-16.png" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
       <Component {...pageProps} />
     </Chakra>
