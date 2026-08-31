66954ad6834092884d31587b45d0cbbc7a9f818b
laphisboy <laphisboy@gmail.com>
2026-08-31 13:28:26 +0900

    fix fauvicon -> png

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,9 @@ function MyApp({ Component, pageProps }) {
         <title>Young Sun Choi - AI Researcher</title>
         <meta name="description" content="Young Sun Choi - Ph.D. student at Yonsei University, 3D computer vision." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon-32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icon-16.png" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
       <Component {...pageProps} />
